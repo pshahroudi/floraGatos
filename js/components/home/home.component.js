b7cf82f57be6437089a11a68e8be012d7d229aa1
Pourya <pshahroudi@gmail.com>
2025-09-02 10:42:17 -0700

    new images added

--- js/components/home/home.component.js
+++ js/components/home/home.component.js
@@ -1,34 +1,32 @@
-(function() {
-  'use strict';
+(function () {
+  "use strict";
 
-  angular
-    .module('floraGatos')
-    .component('home', {
-      templateUrl: 'js/components/home/home.template.html',
-      controller: HomeController
-    });
+  angular.module("floraGatos").component("home", {
+    templateUrl: "js/components/home/home.template.html",
+    controller: HomeController,
+  });
 
   function HomeController() {
     var vm = this;
     vm.categories = [
       {
-        name: 'Terrariums',
-        description: 'Low-maintenance, perfect for desks under LED lights.',
-        image: 'assets/terrarium.svg',
-        alt: 'Glass terrarium on a desk'
+        name: "Terrariums",
+        description: "Low-maintenance, perfect for desks under LED lights.",
+        image: "assets/terrarium_on_desk.png",
+        alt: "Glass terrarium on a desk",
       },
       {
-        name: 'Succulents',
-        description: 'Perfect for window decorations.',
-        image: 'assets/succulent-trio.svg',
-        alt: 'Succulent trio in ceramic pots'
+        name: "Succulents",
+        description: "Perfect for window decorations.",
+        image: "assets/succulents_window.png",
+        alt: "Succulent trio in ceramic pots",
       },
       {
-        name: 'Indoor House Plants',
-        description: 'Vibrant foliage for any room.',
-        image: 'assets/fiddle-leaf-fig.svg',
-        alt: 'Fiddle leaf fig in a living room'
-      }
+        name: "Indoor House Plants",
+        description: "Vibrant foliage for any room.",
+        image: "assets/indoor_house_plants.png",
+        alt: "Fiddle leaf fig in a living room",
+      },
     ];
   }
 })();
