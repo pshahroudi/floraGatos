18d3f44dcb078ccf6bf1e7c50b0e669fe435cf58
Pourya <pshahroudi@email.com>
2025-09-02 23:07:18 -0700

    Changes to the products

--- js/components/terrariums/terrariums.component.js
+++ js/components/terrariums/terrariums.component.js
@@ -11,29 +11,32 @@
 
     vm.products = [
       {
-        name: "Geometric Oasis Terrarium",
-        image: "assets/terrarium_bed.png",
+        name: "Terrarium - Wire jar glass",
+        image: "assets/terrarium_wire_jar_01.jpeg",
         description:
           "A stunning glass terrarium with sharp angles that showcase a modern succulent scene.",
         details:
-          "Crafted with clean lines, this terrarium houses a variety of hardy succulents and moss, creating a vibrant focal point for any room.",
+          "Terrarium with Vertical moss, red lava stone, fittonia and other plants",
+        price: "75.00",
       },
-      {
-        name: "Classic Bell Jar Terrarium",
-        image: "assets/terrarium_kitchen_counter.png",
-        description:
-          "Traditional bell jar filled with lush foliage and natural accents.",
-        details:
-          "A timeless design featuring layered drainage and humidity-loving plants to bring calm to your desk or shelf.",
-      },
-      {
-        name: "Hanging Orb Terrarium",
-        image: "assets/terrarium_red_fittonia_on_desk.png",
-        description:
-          "A whimsical hanging orb perfect for small spaces and air plants.",
-        details:
-          "Suspended elegance showcasing airy tillandsia and decorative stones. Easy to care for and ideal for bright corners.",
-      },
+      // {
+      //   name: "Classic Bell Jar Terrarium",
+      //   image: "assets/terrarium_kitchen_counter.png",
+      //   description:
+      //     "Traditional bell jar filled with lush foliage and natural accents.",
+      //   details:
+      //     "A timeless design featuring layered drainage and humidity-loving plants to bring calm to your desk or shelf.",
+      //   price: "N/A",
+      // },
+      // {
+      //   name: "Hanging Orb Terrarium",
+      //   image: "assets/terrarium_red_fittonia_on_desk.png",
+      //   description:
+      //     "A whimsical hanging orb perfect for small spaces and air plants.",
+      //   details:
+      //     "Suspended elegance showcasing airy tillandsia and decorative stones. Easy to care for and ideal for bright corners.",
+      //   price: "N/A",
+      // },
     ];
 
     vm.selectedProduct = {};
