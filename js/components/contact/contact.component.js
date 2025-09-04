787f11f8c40527e8f041b86e6d0cc6bf666dda05
Pourya <pshahroudi@gmail.com>
2025-09-04 07:56:10 -0700

    style: format contact info paragraphs

--- js/components/contact/contact.component.js
+++ js/components/contact/contact.component.js
@@ -2,6 +2,19 @@
   'use strict';
 
   angular.module('floraGatos').component('contact', {
-    templateUrl: 'js/components/contact/contact.template.html'
+    templateUrl: 'js/components/contact/contact.template.html',
+    controller: ContactController
   });
+
+  function ContactController() {
+    var vm = this;
+    vm.formData = {};
+    vm.sent = false;
+
+    vm.submitForm = function(form) {
+      if (form.$valid) {
+        vm.sent = true;
+      }
+    };
+  }
 })();
